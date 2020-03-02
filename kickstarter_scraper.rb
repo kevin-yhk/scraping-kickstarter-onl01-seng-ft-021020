18128d8001622247084fe39a9beb6d048342735f
Young Hyun (Kevin) Kim <kevinkim.yhk@gmail.com>
2020-03-02 02:12:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,12 +1,13 @@
 # require libraries/modules here
-require "nokogiri"
-require "pry"
+require 'nokogiri'
+require 'pry'
 def create_project_hash
   
   projects = {}
   html = File.read('fixtures/kickstarter.html')
   
   kickstarter = Nokogiri::HTML(html)
-  
-  
+  binding.pry 
 end
+
+create_project_hash
